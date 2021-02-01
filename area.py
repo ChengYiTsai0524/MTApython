e23ad368044085deebb0a811d4d7abca8f73bc8b
ChengYiTsai0524 <78331132+ChengYiTsai0524@users.noreply.github.com>
2021-02-01 10:46:45 +0800

    Add files via upload

---
+++ area.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 10:02:31 2021
+
+@author: MTAEXAM
+"""
+
+height=float(input("上底:"))
+wide=float(input("下底:"))
+high=float(input("高:"))
+area=(height+wide)*high/2
+print("梯形面積:"+str(area))
