c2a5d127e2db2d4eabca674c5550975aceeb4c71
ChengYiTsai0524 <78331132+ChengYiTsai0524@users.noreply.github.com>
2021-02-01 16:55:48 +0800

    Add files via upload

---
+++ Day1-1!!.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 15:45:33 2021
+
+@author: TQC
+"""
+total=1
+a=int(input("階:"))
+for a in range(1,a+1):
+        total*=a
+        
+print(total)
---
+++ Day1-11+~10.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 15:12:40 2021
+
+@author: TQC
+"""
+#數字1~10加法
+n=1
+total=0
+while n<=10:
+    total+=n
+    n+=1
+    print(total)
